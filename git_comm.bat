75a686cba58cf6a548de244c94bccc18890ef3c3
lorenzo <brainstew927@gmail.com>
2019-03-09 17:32:22 +0100

    aggiunto adder git

---
+++ git_comm.bat
@@ -0,0 +1,6 @@
+echo aggiungo tutti i file nella repo
+git add --all
+echo commit dei cambiamenti nella repo locale
+git commit
+echo push della repo locale nella repo remota nella branch exp
+git push gitb exp
